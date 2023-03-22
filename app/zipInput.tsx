84dd7efbe59d815919975c85057fcddfc486fdbe
Chris Cornelius <Cornelius>
2023-03-22 15:28:39 -0500

    Cleanup

--- app/zipInput.tsx
+++ app/zipInput.tsx
@@ -6,7 +6,7 @@ import {useRouter} from "next/navigation";
 export default function ZipInput() {
     const [zipCode, setZipcode] = useState("");
     const router = useRouter();
-    
+
     return (
         <div>
             <label> Enter Zip Code</label>
