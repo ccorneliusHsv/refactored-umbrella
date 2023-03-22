4616cd6c0bcd176b4bfcb9444e14f876e8503e49
Chris Cornelius <Cornelius>
2023-03-22 15:27:15 -0500

    Added ability to use a zip Input component to navigate to pages.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import {ForecastReturn} from "@/app/weatherTypes";
+import ZipInput from "@/app/zipInput";
 
 export default async function Home() {
   return (
     <main>
       <h1> Hello Next 13</h1>
+        <ZipInput/>
     </main>
   )
 }
