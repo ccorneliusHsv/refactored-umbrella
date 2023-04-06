718535d893433c104bc1dc507b6a14df92d3cb75
Chris Cornelius <Cornelius>
2023-04-06 14:29:57 -0500

    Added Tailwind to the project

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import ZipInput from "../components/zipInput";
 export default function Home() {
   return (
       <main>
-        <h1> Hello Next 13</h1>
+        <h1 className="text-3xl font-bold underline"> Hello Next 13</h1>
           <ZipInput/>
       </main>
   )
