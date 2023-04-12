d08c9406871c067d8713e612c72a205565c4feca
Chris Cornelius <Cornelius>
2023-04-12 09:11:50 -0500

    Removed css as it was conflicting with TailWind

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import ZipInput from "../components/zipInput";
 
 export default function Home() {
   return (
-      <main>
+      <main className="bg-stone-500 border-amber-500 h-96">
         <h1 className="text-3xl font-bold underline"> Hello Next 13</h1>
           <ZipInput/>
       </main>
