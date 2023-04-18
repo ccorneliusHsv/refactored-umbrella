d61f5809de752ea840e7e350744301916317c726
Chris Cornelius <Cornelius>
2023-04-18 07:46:56 -0500

    Added a check for a null prop

--- pages/zipcode/ForecastCard.tsx
+++ pages/zipcode/ForecastCard.tsx
@@ -7,10 +7,13 @@ type Props = {
 
 //TODO if we add images add domain to next config file
 export default function ForecastCard( props: Props ) {
+    if(!props.period){
+        return <></>
+    }
     return (
         <div>
             <h3>{props.period.name}</h3>
-            <h4>{"Start time = " + props.period.startTime + "  End Time = " + props.period.endTime}</h4>
+            <h4>{"Start time = " + props.period?.startTime + "  End Time = " + props.period.endTime}</h4>
             <h4>{props.period.detailedForecast}</h4>
             <br/>
             <br/>
